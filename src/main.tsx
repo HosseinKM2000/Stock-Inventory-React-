c7860180c0f9a3079e44652c761a9118b33fbc14
Hossein_Kamari <111495578+HosseinKM2000@users.noreply.github.com>
2026-05-18 12:26:57 +0330

    removing irrelevant codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,24 @@
-import { bootstrap } from "./bootstrap/app";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-bootstrap();
+import "@radix-ui/themes/styles.css";
+import "./index.css"
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Theme appearance="dark">
+      <RouterProvider router={router} />
+    </Theme>
+  </React.StrictMode>,
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
-import { keycloak } from "src/core/auth/keycloak";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    if (!keycloak.authenticated) {
-      throw redirect({
-        to: "/login",
-      });
-    }
-  },
   component: RouteComponent,
 });
 
